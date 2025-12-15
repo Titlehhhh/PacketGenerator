4994445d2b20de1f1469fbe427f2a69bef566d3e
Titlehhhh <93156853+Titlehhhh@users.noreply.github.com>
2025-12-15 20:26:51 +0500

    Improve structural comparison of protocol types

--- src/PacketGenerator/Utils.fs
+++ src/PacketGenerator/Utils.fs
@@ -1,6 +1,7 @@
-﻿namespace PacketGenerator.Utils
+namespace PacketGenerator.Utils
 
 open System
+open System.Text.Json.Nodes
 open Protodef
 open PacketGenerator.Types
 
@@ -10,7 +11,15 @@ module VersionRange =
 module Util =
     let equalTwoTypes (t1: ProtodefType option) (t2: ProtodefType option) =
         match t1, t2 with
-        | Some t1, Some t2 -> t1.Equals(t2)
+        | Some t1, Some t2 when Object.ReferenceEquals(t1, t2) -> true
+        | Some t1, Some t2 ->
+            let json1 = t1.ToJson()
+            let json2 = t2.ToJson()
+
+            match JsonNode.Parse(json1), JsonNode.Parse(json2) with
+            | null, null -> true
+            | node1, node2 when isNull node1 || isNull node2 -> false
+            | node1, node2 -> node1.DeepEquals(node2)
         | None, None -> true
         | _ -> false
 
